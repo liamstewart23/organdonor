8a261533bbfe6b83a0dfa4938bacd47f5478b5c0
laurenkoza <l_koza3@fanshaweonline.ca>
2017-02-17 00:18:27 -0500

    Home banner SVGs animated on scroll

--- js/app.js
+++ js/app.js
@@ -52,6 +52,64 @@ app.controller('HomeCtrl', [function() {
         $('#home1to8').css({'height':(($(window).height()/3))+'px'});
         $('#home3days').css({'height':(($(window).height()/3))+'px'});
     });
+
+    //Icon Animations on Scroll
+
+    var lightbulb = document.querySelector("#iconLightbulb");
+    var lightBanner = document.querySelector("#banner3");
+
+    var community = document.querySelector("#iconCommunity");
+    var comBanner = document.querySelector("#banner4");
+    var a = 0;
+    var b = 0;
+
+    //Lightbulb
+    function lightbulbLoaded(e) {
+        var lightbulbHover = e.currentTarget.contentDocument;
+        var lightbulbIcon = lightbulbHover.querySelector("#lighticon");
+        var lightbulbInd = lightbulbHover.querySelector("#lightbright");
+
+        window.addEventListener("scroll", brighten, false);
+
+        function brighten(scroll){
+            var trigger = window.scrollY + window.innerHeight;
+            if(lightBanner.offsetTop + 200 < trigger){
+                a++;
+                if(a===1){
+                    TweenMax.to(lightbulbInd, .4, {scale: .9, transformOrigin: "50% 100%", ease:Power2.easeInOut});
+                    TweenMax.to(lightbulbInd, .5, {delay: .4, scale: 1, ease:Power2.easeIn});
+                }
+            }
+        }
+    }
+    //Community
+    function communityLoaded(e) {
+        var comHover = e.currentTarget.contentDocument;
+        var comIcon = comHover.querySelector("#comHands");
+        var comInd = comHover.querySelector("#heart");
+
+        window.addEventListener("scroll", heartbeat, false);
+
+        function heartbeat(scroll){
+            var trigger = window.scrollY + window.innerHeight;
+            console.log(trigger);
+            if(comBanner.offsetTop + 200 < trigger){
+                console.log('trigger');
+                b++;
+                if(b===1){
+                    TweenMax.to(comInd, .4, {scale: 1.15, transformOrigin: "50% 100%", yoyo: true, repeat: 3, ease:Power2.easeInOut});
+                    TweenMax.to(comInd, .5, {delay: 1.5, scale: 1, ease:Power2.easeIn});
+                }
+            }
+        }
+    }
+
+    lightbulb.addEventListener("load", lightbulbLoaded, false);
+    community.addEventListener("load", communityLoaded, false);
+    
+
+
+
     });
 }]);
 //Controller for Learn
