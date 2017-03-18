be4a22e700dc7528fccf5d47ca30b1baf48fa612
laurenkoza <l_koza3@fanshaweonline.ca>
2017-03-18 18:09:30 -0400

    can now add stories to db. db added to db/

--- admin/phpscripts/queries.php
+++ admin/phpscripts/queries.php
@@ -12,5 +12,20 @@
 		}else{
 			return $error;
 		}
+	mysqli_close($link);
+	}
+
+	function addStory($name,$age,$city,$organ,$story,$video){
+		$type = 'written';
+		require_once('config.php');
+		$query = "INSERT INTO tbl_stories VALUES(NULL,'{$name}','{$age}','{$organ}','{$city}','{$type}','{$story}','{$video}')";
+		$run = mysqli_query($link, $query);
+
+		if($run){
+			redirect_to('admin_index.php');
+		}else{
+			return $error;
+		}
+	mysqli_close($link);
 	}
 ?>
