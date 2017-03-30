38d46f427151c089ff94e94d698f37d6908830af
Liam Stewart <lstewart2397@gmail.com>
2017-03-30 16:30:07 -0400

    updates

--- partials/home.php
+++ partials/home.php
@@ -24,15 +24,11 @@
 		<img src="img/icons/community.svg" height="0" alt="Because a Donor" id="iconHeart">
 		<h4>Many Canadians are alive today <br><span class="red">Because of an Organ Donor</span></h4>
 		<div class="col-xs-12 col-md-8 col-md-offset-2">
-			<p>Because A Donor is inspired by the true stories of organ recipients and donors across Ontario. By reaching out to the community, we will create a network of story-telling that spreads awareness, humanity, and positivity in the face of very challenging situations.</p>
-			<p>Behind Because A Donor is the idea that anyone, at any point in time, may find
-				themselves in need of a life-saving organ. For this campaign, we ask what are the
-				extraordinary, the everyday, the unexpectedly precious moments that can be
-			experienced.</p>
-		</div>
-		<div class="col-xs-12 col-md-12">
-			<p>Tell us yours.</p>
-			<h5>#because<span class="red">a</span>donor</h5>
+			<p>We have created a network that has allowed recipients, donors, and their families all across Ontario to share their stories.</p>
+			<div class="col-xs-12 col-md-12">
+				<p>Tell us yours.</p>
+				<h5>#because<span class="red">a</span>donor</h5>
+			</div>
 		</div>
 	</div>
 	<div class="col-xs-12 col-sm-offset-0 col-md-6 text-center homeBannerText" id="bannerHome3">
